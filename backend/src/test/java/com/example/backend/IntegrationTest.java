5b0fb2975cdfd9b89c2fc6cbff7a2404d6081049
sonia <sofiashchukina@outlook.com>
2022-07-15 18:02:48 +0200

    write all integration tests

--- backend/src/test/java/com/example/backend/IntegrationTest.java
+++ backend/src/test/java/com/example/backend/IntegrationTest.java
@@ -5,12 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -18,6 +18,7 @@ class IntegrationTest {
     @Autowired
     MockMvc mockMvc;
 
+    @DirtiesContext
     @Test
     void showAllTasks() throws Exception {
         mockMvc.perform(post("/api/todo")
@@ -26,7 +27,7 @@ class IntegrationTest {
                                 "status":"OPEN"}
                                 """)
                         .contentType(MediaType.APPLICATION_JSON))
-                                .andExpect(status().isOk());
+                .andExpect(status().isOk());
 
         mockMvc.perform(post("/api/todo")
                         .content("""
@@ -37,15 +38,15 @@ class IntegrationTest {
                 .andExpect(status().isOk());
 
         mockMvc.perform(get("/api/todo"))
-                        .andExpect(status().isOk())
-                        .andExpect(content().json("""
-                                [{"description":"swim",
-                                "status":"OPEN"}, {"description":"run",
-                                "status":"OPEN"}]
-                                """));
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        [{"description":"swim",
+                        "status":"OPEN"}, {"description":"run",
+                        "status":"OPEN"}]
+                        """));
 
     }
-
+    @DirtiesContext
     @Test
     void showTaskByID() throws Exception {
         mockMvc.perform(post("/api/todo")
@@ -67,30 +68,105 @@ class IntegrationTest {
         mockMvc.perform(get("/api/todo/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().json("""
-                                {"description":"swim",
-                                "status":"OPEN"}
-                                """));
+                        {"description":"swim",
+                        "status":"OPEN"}
+                        """));
         mockMvc.perform(get("/api/todo/2"))
                 .andExpect(status().isOk())
                 .andExpect(content().json("""
-                                {"description":"run",
-                                "status":"OPEN"}
-                                """));
+                        {"description":"run",
+                        "status":"OPEN"}
+                        """));
 
 
     }
 
-
+    @DirtiesContext
     @Test
     void changeStatus() throws Exception {
+        mockMvc.perform(post("/api/todo")
+                        .content("""
+                                {"description":"swim",
+                                "status":"OPEN"}
+                                """)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(put("/api/todo/{id}/update", 1)
+                        .content("""
+                                {"description":"swim",
+                                "status":"IN_PROGRESS"}
+                                """)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/api/todo/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {"description":"swim",
+                        "status":"IN_PROGRESS"}
+                        """));
 
     }
-
+    @DirtiesContext
     @Test
-    void deleteTaskByID() {
+    void deleteTaskByID() throws Exception {
+        mockMvc.perform(post("/api/todo")
+                        .content("""
+                                {"description":"swim",
+                                "status":"OPEN"}
+                                """)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(post("/api/todo")
+                        .content("""
+                                {"description":"run",
+                                "status":"OPEN"}
+                                """)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(delete("/api/todo/1"))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/api/todo"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        [ {"description":"run",
+                        "status":"OPEN"}]
+                        """));
+
     }
-
+    @DirtiesContext
     @Test
-    void advanceStatus() {
+    void advanceStatus() throws Exception {
+        mockMvc.perform(post("/api/todo")
+                        .content("""
+                                {"description":"swim",
+                                "status":"OPEN"}
+                                """)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(put("/api/todo/{id}/", 1))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/api/todo/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {"description":"swim",
+                        "status":"IN_PROGRESS"}
+                        """));
+        mockMvc.perform(put("/api/todo/{id}/", 1))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/api/todo/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {"description":"swim",
+                        "status":"DONE"}
+                        """));
+
     }
 }
